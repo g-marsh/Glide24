004e091b4a1134342a61f323d0de13926910523c
marshg <iltrmb@gmail.com>
2016-09-15 16:32:35 -0500

    debug test

--- app/src/main/java/com/gregmarsh/glide24/MainActivity.java
+++ app/src/main/java/com/gregmarsh/glide24/MainActivity.java
@@ -16,7 +16,12 @@ import com.bumptech.glide.load.resource.bitmap.VideoBitmapDecoder;
 public class MainActivity extends AppCompatActivity {
 
     EditText editText;
+    EditText editText2;
     ImageView imageView;
+    ImageView imageView2;
+    ImageView imageView3;
+    Uri videoUri;
+    Uri pictureUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +30,40 @@ public class MainActivity extends AppCompatActivity {
 
 
         editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
         imageView = (ImageView) findViewById(R.id.imageView);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+        imageView3 = (ImageView) findViewById(R.id.imageView3);
 
         String videoName1 = Environment.getExternalStorageDirectory().getPath() + "/DCIM/" + editText.getText() + ".mp4";
+        String pictureName = Environment.getExternalStorageDirectory().getPath() + "/DCIM/" + editText2.getText();
+        videoUri = Uri.parse(videoName1);
+        pictureUri = Uri.parse(pictureName);
 //        videoView.setVideoURI(Uri.parse(videoName1));
 
+//        Glide
+//                .with(getApplicationContext())
+//                .load(videoUri)
+//                .into(imageView);
+
+        Glide.with(this).load("http://goo.gl/gEgYUd").into(imageView2);
 
 
-        BitmapPool bitmapPool = Glide.get(getApplicationContext()).getBitmapPool();
-        int microSecond = 6000000;// 6th second as an example
-        VideoBitmapDecoder videoBitmapDecoder = new VideoBitmapDecoder(microSecond);
-        FileDescriptorBitmapDecoder fileDescriptorBitmapDecoder = new FileDescriptorBitmapDecoder(videoBitmapDecoder, bitmapPool, DecodeFormat.PREFER_ARGB_8888);
-        Glide.with(getApplicationContext())
-                .load(Uri.parse(videoName1))
-                .asBitmap()
-                .override(50,50)// Example
-                .videoDecoder(fileDescriptorBitmapDecoder)
-                .into(imageView);
+
+//        imageView3.setImageResource(R.drawable.mpi1);
+        imageView3.setImageURI(pictureUri);
+
+
+//        BitmapPool bitmapPool = Glide.get(getApplicationContext()).getBitmapPool();
+//        int microSecond = 6000000;// 6th second as an example
+//        VideoBitmapDecoder videoBitmapDecoder = new VideoBitmapDecoder(microSecond);
+//        FileDescriptorBitmapDecoder fileDescriptorBitmapDecoder = new FileDescriptorBitmapDecoder(videoBitmapDecoder, bitmapPool, DecodeFormat.PREFER_ARGB_8888);
+//        Glide.with(getApplicationContext())
+//                .load(videoUri)
+//                .asBitmap()
+//                .override(50,50)// Example
+//                .videoDecoder(fileDescriptorBitmapDecoder)
+//                .into(imageView);
 
         //        Glide.with(getApplicationContext())
         //                .load(yourUri)
@@ -49,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
 //                .override(50,50)// Example
 //                .videoDecoder(fileDescriptorBitmapDecoder)
 //                .into(yourImageView);
+
 
 
 
